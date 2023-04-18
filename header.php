aee0da6ebe98f123bb466ec12a9f41fb9a4b3dd2
Creastv <piotr@roial.pl>
2023-04-18 10:52:45 +0200

    fixing

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta name="theme-color" content="#575289">
+    <meta name="theme-color" content="#0d559c">
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
